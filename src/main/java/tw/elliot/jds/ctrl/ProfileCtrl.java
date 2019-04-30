6de86798872da94dd40ad1ac5a5107a91cb31ced
Elliot Chen <elliot.chen@elliot.tw>
2019-04-30 13:56:11 +0800

    support docker

--- src/main/java/tw/elliot/jds/ctrl/ProfileCtrl.java
+++ src/main/java/tw/elliot/jds/ctrl/ProfileCtrl.java
@@ -1,5 +1,6 @@
 package tw.elliot.jds.ctrl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/profile/*")
+@Slf4j
 public class ProfileCtrl {
 
 	@Value("${app.profile}")
@@ -16,6 +18,7 @@ public class ProfileCtrl {
 
 	@GetMapping("checkProfile")
 	public String checkProfile(HttpSession session) {
+		log.warn("Get Session [{}]", session.getId());
 		return profile;
 	}
 }
